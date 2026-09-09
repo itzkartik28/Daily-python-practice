b8b2bb6a9a1b8ba70bc942bfd9a747ec7e7f0407
Kartik Salunkhe <kartiksalunkhe2801@gmail.com>
2026-09-09 11:30:14 +0530

    Add files via upload

---
+++ append method for list.py
@@ -0,0 +1,6 @@
+fruits=["mango","banana"]
+
+fruits.append("orange")
+fruits.append("chikku")
+
+print(f"update list{fruits}")
---
+++ both dimention.py
@@ -0,0 +1,17 @@
+"""
+Dimention 2 types:
+
+two parts
+   ^
+ 1D 2D
+
+ syntax:
+
+1D =  array name=np.array[value]
+
+2D = array name=np.array([[row],[col])
+
+"""
+
+import numpy as np
+
