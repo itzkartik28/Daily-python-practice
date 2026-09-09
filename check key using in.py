bb583390eb45b071bead98981d8c3ce5fd86f364
Kartik Salunkhe <kartiksalunkhe2801@gmail.com>
2026-09-09 11:31:30 +0530

    Add files via upload

---
+++ check key using in.py
@@ -0,0 +1,6 @@
+students={
+    101:"rahul",
+    102:"amit"
+}
+
+print(F"check key is there or not:{103 in students}")
---
+++ count students.py
@@ -0,0 +1,10 @@
+students={
+    101:"rahul",
+    102:"amit"
+}
+count=0
+
+for i in students:
+    count+=1
+
+print(f"total students:{count}")
